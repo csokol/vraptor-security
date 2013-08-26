45a29b72f985621b1b50106f18869eaaa6e19f60
Francisco Sokol <chico.sokol@gmail.com>
2013-08-26 16:27:24 -0300

    controle basico funcionando, precisa fazer página de login

--- src/main/java/br/com/caelum/vraptor/security/controller/HomeController.java
+++ src/main/java/br/com/caelum/vraptor/security/controller/HomeController.java
@@ -2,13 +2,20 @@ package br.com.caelum.vraptor.security.controller;
 
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.security.dao.UserDao;
 
 @Resource
 public class HomeController {
 	
-	@Get("/")
+	private UserDao dao;
+
+	public HomeController(UserDao dao) {
+		this.dao = dao;
+	}
+	
+	@Get("/security/")
 	public void index() {
-		System.out.println("hello world");
+		System.out.println("hello world " + dao);
 	}
 
 }
---
+++ src/main/java/br/com/caelum/vraptor/security/dao/UserDao.java
@@ -0,0 +1,8 @@
+package br.com.caelum.vraptor.security.dao;
+
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+public class UserDao {
+
+}
